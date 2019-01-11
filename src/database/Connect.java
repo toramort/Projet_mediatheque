8cc5f67d7235c475b2bbee3d3a495be597293948
Samuel Bagattin <samuel.bagattin@gmail.com>
2019-01-11 11:47:36 +0100

    Ajout oeuvre v1 fixed database import

---
+++ src/database/Connect.java
@@ -0,0 +1,25 @@
+package database;
+
+import javax.swing.*;
+import java.sql.Connection;
+
+import static java.sql.DriverManager.getConnection;
+
+public class Connect {
+    private static Connection conn;
+
+    public Connect(String driver, String url, String user, String passwd) {
+
+        conn = null;
+        try {
+            conn = getConnection(url, user, passwd);
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Connection to database failed", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public static Connection getConn() {
+        return conn;
+    }
+}
